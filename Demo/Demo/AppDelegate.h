3756c722125cb65dfa8a2afbc436de6a50f74f54
HaiyanLiang <835532786@qq.com>
2017-05-15 14:01:13 +0800

    DEMO

---
+++ Demo/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by 梁海雁 on 17/5/15.
+//  Copyright © 2017年 梁海雁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by 梁海雁 on 17/5/15.
+//  Copyright © 2017年 梁海雁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
